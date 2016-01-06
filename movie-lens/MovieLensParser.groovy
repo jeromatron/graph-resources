eba6678fc44bd9f28e53e856daa3c1eaeebc528c
Jeremy Hanna <jhanna@datastax.com>
2016-01-06 16:06:48 -0600

    Updated the script to not have to have a TinkerGraph index so that the script can be used on other graph dbs more easily.  Made it so all of Marko's presentation examples will work but added some clarification.

--- movie-lens/MovieLensParser.groovy
+++ movie-lens/MovieLensParser.groovy
@@ -1,8 +1,7 @@
 import org.apache.tinkerpop.gremlin.structure.Graph
-import org.apache.tinkerpop.gremlin.structure.Vertex
 
 /**
- * See https://gist.github.com/dkuppitz/64a9f1ba30ca652d067a
+ * See https://gist.github.com/okram/d9f158dee789689759da and https://gist.github.com/dkuppitz/64a9f1ba30ca652d067a
  * Dataset: http://grouplens.org/datasets/movielens/1m/
  *
  * gremlin> :load /tmp/MovieLensParser.groovy
@@ -10,10 +9,12 @@ import org.apache.tinkerpop.gremlin.structure.Vertex
  * gremlin> graph = TinkerGraph.open()
  * ==>tinkergraph[vertices:0 edges:0]
  * gremlin> MovieLensParser.load(graph, '/tmp/ml-1m')
- * Processing movies.dat...
+ * Adding occupation vertices...
+ * Adding categories from movies.dat...
+ * Adding movies from movies.dat...
  * Processing users.dat...
  * Processing ratings.dat...
- * Loading took (ms): 14273
+ * Loading took (ms): 12116
  * ==>null
  * gremlin> graph.io(IoCore.gryo()).writeGraph("/tmp/movie-lens.kryo")
  * ==>null
@@ -24,76 +25,66 @@ class MovieLensParser {
     static Map occupations
 
     static {
-        occupations = [0: "other", 1: "academic/educator", 2: "artist",
-                       3: "clerical/admin", 4: "college/grad student", 5: "customer service",
-                       6: "doctor/health care", 7: "executive/managerial", 8: "farmer",
-                       9: "homemaker", 10: "K-12 student", 11: "lawyer", 12: "programmer",
-                       13: "retired", 14: "sales/marketing", 15: "scientist", 16: "self-employed",
-                       17: "technician/engineer", 18: "tradesman/craftsman", 19: "unemployed", 20: "writer"]
+        occupations = [0 : 'other', 1: 'academic/educator', 2: 'artist',
+                       3 : 'clerical/admin', 4: 'college/grad student', 5: 'customer service',
+                       6 : 'doctor/health care', 7: 'executive/managerial', 8: 'farmer',
+                       9 : 'homemaker', 10: 'K-12 student', 11: 'lawyer', 12: 'programmer',
+                       13: 'retired', 14: 'sales/marketing', 15: 'scientist', 16: 'self-employed',
+                       17: 'technician/engineer', 18: 'tradesman/craftsman', 19: 'unemployed', 20: 'writer']
     }
 
     public static void parse(final Graph graph, final String dataDirectory) {
 
         def g = graph.traversal()
 
-        println 'Processing movies.dat...'
+        println "Adding occupation vertices..."
+        occupations.each {
+            key, value ->
+                graph.addVertex(id, 'o' + key, label, 'occupation', 'name', value)
+        }
+
+        println "Adding categories from movies.dat..."
+        def categories = [] as Set
+        new File(dataDirectory + '/movies.dat').eachLine {
+            it.split("::")[2].split("\\|").each { categories.add(it) }
+        }
+        categories.each {
+            graph.addVertex(label, 'category', 'name', it)
+        }
+
+        println 'Adding movies from movies.dat...'
         // MovieID::Title::Genres
-        new File(dataDirectory + '/movies.dat').eachLine { final String line ->
-
-            def components = line.split("::")
-            def movieId = components[0].toInteger()
-            def movieTitleYear = components[1] =~ /(.*\b)\s*\((\d+)\)/
-
-            if (!movieTitleYear.find()) return
-
-            def movieTitle = movieTitleYear.group(1)
-            def movieYear = movieTitleYear.group(2).toInteger()
-            def genres = components[2]
-            def movieVertex = graph.addVertex(label, 'movie', 'uid', 'm' + movieId, 'movieId', movieId, 'name', movieTitle, 'year', movieYear)
-
-            genres.split('\\|').each { def genre ->
-                def genreVertex = g.V().has('uid', 'g' + genre).tryNext().orElseGet {graph.addVertex(label, 'genre', 'uid', 'g' + genre, 'name', genre)}
-                movieVertex.addEdge('hasGenre', genreVertex)
-            }
+        new File(dataDirectory + '/movies.dat').eachLine {
+            line ->
+                def components = line.split("::")
+                def movieId = new Integer(components[0])
+                def movieTitleYear = components[1] =~ /(.*)\s*\((\d+)\)/
+                movieTitleYear.find()
+                def movie = graph.addVertex(id, 'm' + movieId, label, 'movie', 'name', movieTitleYear.group(1).trim(), 'year', movieTitleYear.group(2) as Integer)
+                components[2].split("\\|").each {
+                    movie.addEdge('category', g.V().hasLabel('category').has('name', it).next())
+                }
         }
 
         println 'Processing users.dat...'
         // UserID::Gender::Age::Occupation::Zip-code
-        new File(dataDirectory + '/users.dat').eachLine { final String line ->
-
-            def components = line.split("::")
-            def userId = components[0].toInteger()
-            def userGender = components[1]
-            def userAge = components[2].toInteger()
-            def occupationId = components[3].toInteger()
-            def userZipcode = components[4]
-            def userVertex = graph.addVertex(label, 'person', 'uid', 'u' + userId, 'userId', userId, 'gender', userGender, 'age', userAge, 'zipcode', userZipcode)
-
-            def occupationVertex = g.V().has('uid', 'o' + occupationId).tryNext().orElseGet {
-                graph.addVertex(label, 'occupation', 'uid', 'o' + occupationId, 'jobId', occupationId, 'name', occupations.get(occupationId))
-            }
-
-            userVertex.addEdge('hasOccupation', occupationVertex)
+        new File(dataDirectory + '/users.dat').eachLine {
+            def user = graph.addVertex(id, 'u' + (it.split("::")[0] as Integer), label, 'user', 'gender', it.split("::")[1], 'age', (it.split("::")[2] as Integer))
+            user.addEdge('occupation', g.V().hasLabel('occupation').has(id, 'o' + it.split("::")[3]).next())
         }
 
         println 'Processing ratings.dat...'
         // UserID::MovieID::Rating::Timestamp
-        new File(dataDirectory + '/ratings.dat').eachLine { final String line ->
-            def components = line.split("::")
-            def userId = components[0].toInteger()
-            def movieId = components[1].toInteger()
-            def stars = components[2].toInteger()
-            def time = components[3].toLong()
-            def userTraversal = g.V().has('uid', 'u' + userId)
-            def movieTraversal = g.V().has('uid', 'm' + movieId)
-            if (userTraversal.hasNext() && movieTraversal.hasNext()) {
-                userTraversal.next().addEdge('rated', movieTraversal.next(), 'stars', stars, 'time', time)
-            }
+        new File(dataDirectory + '/ratings.dat').eachLine {
+            line ->
+                def components = line.split("::");
+                def user = g.V('u' + (components[0] as Integer)).next()
+                def movie = g.V('m' + (components[1] as Integer)).next()
+                user.addEdge('rated', movie, 'stars', components[2] as Integer)
         }
     }
 
     public static void load(final Graph graph, final String dataDirectory) {
-        graph.createIndex('uid', Vertex.class)
         def start = System.currentTimeMillis()
         parse(graph, dataDirectory)
         println "Loading took (ms): " + (System.currentTimeMillis() - start)
--- movie-lens/README.md
+++ movie-lens/README.md
@@ -28,7 +28,9 @@ gremlin> :load MovieLensParser.groovy
 gremlin> graph = TinkerGraph.open()
 ==>tinkergraph[vertices:0 edges:0]
 gremlin> MovieLensParser.load(graph, '/tmp/ml-1m')
-Processing movies.dat...
+Adding occupation vertices...
+Adding categories from movies.dat...
+Adding movies from movies.dat...
 Processing users.dat...
 Processing ratings.dat...
 Loading took (ms): 14273
@@ -45,14 +47,14 @@ gremlin> graph = TinkerGraph.open()
 gremlin> graph.io(gryo()).readGraph('/tmp/movie-lens.kryo')
 ==>null
 gremlin> g = graph.traversal()
-==>graphtraversalsource[tinkergraph[vertices:9625 edges:969719], standard]
+==>graphtraversalsource[tinkergraph[ertices:9962 edges:1012657], standard]
 gremlin> g.V().count
 No such property: count for class: org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.DefaultGraphTraversal
 Display stack trace? [yN] 
 gremlin> g.V().count()
-==>9625
+==>9962
 gremlin> g.E().count()
-==>969719
+==>1012657
 gremlin> 
 ```
 
@@ -66,10 +68,10 @@ g.V().has('movie', 'name', 'Toy Story').inE('rated').values('stars').mean()
 Or get the list of genres:
 
 ```
-g.V().hasLabel('genre').valueMap()
+g.V().hasLabel('category').valueMap()
 ```
 
 References
 ----------
 
-- Original script: [https://gist.github.com/dkuppitz/64a9f1ba30ca652d067a](https://gist.github.com/dkuppitz/64a9f1ba30ca652d067a)
+- See https://gist.github.com/okram/d9f158dee789689759da and https://gist.github.com/dkuppitz/64a9f1ba30ca652d067a
